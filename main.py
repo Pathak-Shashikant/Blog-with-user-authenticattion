cc35d4ac8c10e23235f042448bdb69a4057b04ef
shashi <shashikp911@gmail.com>
2022-02-20 19:07:05 +0530

    initial commit on Feb 20, 2022

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ class Comment(db.Model):
     parent_post = relationship("BlogPost", back_populates="comments")
     text = db.Column(db.Text, nullable=False)
 
-# db.create_all()
+db.create_all()
 
 @login_manager.user_loader
 def load_user(user_id):
